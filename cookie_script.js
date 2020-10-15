deda4fc53955ee62223cf5b4f53407676b471b51
ManuMoreno84 <70131058+ManuMoreno84@users.noreply.github.com>
2020-10-15 09:53:35 +0200

    Add files via upload

---
+++ cookie_script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('execute').addEventListener('click',()=> {
+const NameInput = document.getElementById('name').value;
+
+document.cookie1 = NameInput
+console.log(document.cookie1)
+});
+
